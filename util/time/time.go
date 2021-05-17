1762c3a5c5403c8afa0ff88994505dbd729ca47b
uhhc <springshinehoo@gmail.com>
2021-05-17 18:22:16 +0800

    Update time util

--- util/time/time.go
+++ util/time/time.go
@@ -13,9 +13,10 @@ const (
 	Minute string = "04"
 	Second string = "05"
 
-	DateSlashFormat = Year + "/" + Month + "/" + Day
-	FullTimeFormat  = Year + "-" + Month + "-" + Day + " " + Hour + ":" + Minute + ":" + Second
-	QueryFormat     = Year + "-" + Month + "-" + Day
+	DateSlashFormat            = Year + "/" + Month + "/" + Day
+	FullTimeFormat             = Year + "-" + Month + "-" + Day + " " + Hour + ":" + Minute + ":" + Second
+	FullTimeWithNoSecondFormat = Year + "-" + Month + "-" + Day + " " + Hour + ":" + Minute
+	QueryFormat                = Year + "-" + Month + "-" + Day
 )
 
 // GetDateSlashFormatTime to get the time format for DateSlashFormat
@@ -44,6 +45,19 @@ func GetFullTimeFormatFromTimePointer(t *time.Time) string {
 	return ""
 }
 
+// GetFullTimeWithNoSecondFormat to get the full time with no second format
+func GetFullTimeWithNoSecondFormat(t time.Time) string {
+	return t.Local().Format(FullTimeWithNoSecondFormat)
+}
+
+// GetFullTimeWithNoSecondFormatFromTimePointer to get the full time format
+func GetFullTimeWithNoSecondFormatFromTimePointer(t *time.Time) string {
+	if t != nil {
+		return t.Local().Format(FullTimeWithNoSecondFormat)
+	}
+	return ""
+}
+
 // GetQueryFormat to get the time format for query
 func GetQueryFormat(t time.Time) string {
 	return t.Local().Format(QueryFormat)
@@ -65,6 +79,11 @@ func ConvertStrToDateSlashFormat(t string) *time.Time {
 // ConvertFullTimeFormat to convert FullTimeFormat str to time pointer
 func ConvertFullTimeFormat(t string) *time.Time {
 	return convertTime(t, FullTimeFormat)
+}
+
+// ConvertFullTimeWithNoSecondFormat to convert FullTimeFormat str to time pointer
+func ConvertFullTimeWithNoSecondFormat(t string) *time.Time {
+	return convertTime(t, FullTimeWithNoSecondFormat)
 }
 
 // ConvertStrToTime to convert string to time pointer
